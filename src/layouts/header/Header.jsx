ba6df22ee1534312f142de92a262150b626b5d81
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2025-10-09 14:07:28 +0500

    header update

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import './HeaderStyle.scss'
 
 const Header = () => {
 	return (
-		<div>Header</div>
+		// Header start
+		<div className='container'>
+			<header className='header'>
+				{/* Header Logo */}
+				<span className='header_logo'>Kashkadarya travel</span>
+				{/* Navbar start */}
+				<nav className='navbar'>
+					<ol className='nav_list'>
+						<li className='nav_item'>Home</li>
+						<li className='nav_item'>About Us</li>
+						<li className='nav_item'>Destinations</li>
+						<li className='nav_item'>Tours</li>
+						<li className='nav_item'>Shop</li>
+						<li className='nav_item'>Blogs</li>
+					</ol>
+				</nav>
+				{/* Navbar end */}
+
+				{/* Login buttons */}
+				<div className='login_btn'>
+					<button className='signIn_btn'>Sign In</button>
+					<button className='signUp_btn'>Sign Up</button>
+				</div>
+			</header>
+		</div>
+		// Header end
 	)
 }
 

82fe7e03485d1ffe52005a13494ced5dc99cb269
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2025-10-09 15:24:32 +0500

    Header update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import Header from '../src/layouts/header/Header'
-import Footer from './layouts/footer/Footer'
-import "./App.scss"
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import './App.scss'
 import Layout from './layouts/layout/Layout'
 
-const App = () => {
-	return <div>
-    <Layout>
-      <h1>Salom</h1>
+// Sahifalar
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Destinations from "./pages/Destinations";
+// import Tours from "./pages/Tours";
+// import Shop from "./pages/Shop";
+// import Blogs from "./pages/Blogs";
+// import SignIn from "./pages/SignIn";
+// import SignUp from "./pages/SignUp";
 
-    </Layout>
-  </div>
+const App = () => {
+	return (
+		<Router>
+			<Layout>
+				<Routes>
+					<Route path='/' element={<Layout><h1>salom</h1></Layout>} />
+				</Routes>
+			</Layout>
+		</Router>
+	)
 }
 
 export default App
--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './HeaderStyle.scss'
 
 const Header = () => {
@@ -6,24 +7,44 @@ const Header = () => {
 		<div className='container'>
 			<header className='header'>
 				{/* Header Logo */}
-				<span className='header_logo'>Kashkadarya travel</span>
+
+				<Link className='header_logo' to='/'>
+					Kashkadarya Travel
+				</Link>
+
 				{/* Navbar start */}
 				<nav className='navbar'>
 					<ol className='nav_list'>
-						<li className='nav_item'>Home</li>
-						<li className='nav_item'>About Us</li>
-						<li className='nav_item'>Destinations</li>
-						<li className='nav_item'>Tours</li>
-						<li className='nav_item'>Shop</li>
-						<li className='nav_item'>Blogs</li>
+						<li className='nav_item'>
+							<Link className='nav_item_link' to='/'>Home</Link>
+						</li>
+						<li className='nav_item'>
+							<Link className='nav_item_link' to='/about'>About Us</Link>
+						</li>
+						<li className='nav_item'>
+							<Link className='nav_item_link' to='/destinations'>Destinations</Link>
+						</li>
+						<li className='nav_item'>
+							<Link className='nav_item_link' to='/tours'>Tours</Link>
+						</li>
+						<li className='nav_item'>
+							<Link className='nav_item_link' to='/shop'>Shop</Link>
+						</li>
+						<li className='nav_item'>
+							<Link className='nav_item_link' to='/blogs'>Blogs</Link>
+						</li>
 					</ol>
 				</nav>
 				{/* Navbar end */}
 
 				{/* Login buttons */}
 				<div className='login_btn'>
-					<button className='signIn_btn'>Sign In</button>
-					<button className='signUp_btn'>Sign Up</button>
+					<Link to='/signin' className='signIn_btn'>
+						Sign In
+					</Link>
+					<Link to='/signup' className='signUp_btn'>
+						Sign Up
+					</Link>
 				</div>
 			</header>
 		</div>
